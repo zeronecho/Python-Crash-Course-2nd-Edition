22a4a0c2ad38a73b241eb48f10cbf9b0b9595c53
zeronecho <zeronecho@outlook.com>
2022-12-21 19:37:15 +0800

    添加练习3-2:问候语

---
+++ Chapter3/3-2.py
@@ -0,0 +1,13 @@
+
+# 练习3-2:问候语
+
+names=['evan','echo','li']
+
+# 两种列表使用方法：正序+倒序
+print(f"Hello, {names[0].title()}, how are you?")
+print(f"Hello, {names[1].title()}, how are you?")
+print(f"Hello, {names[2].title()}, how are you?\n")
+
+print(f"Hello, {names[-3].title()}, how are you?")
+print(f"Hello, {names[-2].title()}, how are you?")
+print(f"Hello, {names[-1].title()}, how are you?")
