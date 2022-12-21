09b829e8cb4d468f50654fa2605a866a4b230893
zeronecho <zeronecho@outlook.com>
2022-12-21 13:36:36 +0800

    练习更新

--- Chapter2/2-3.py
+++ Chapter2/2-3.py
@@ -1,5 +1,5 @@
 
-# 练习2-3
+# 练习2-3 个性化消息
 
 name="echo"
 print("Hello "+name+", would you like to learn some Python today?")
--- Chapter2/2-4.py
+++ Chapter2/2-4.py
@@ -1,7 +1,15 @@
 
-# 练习2-4
+# 练习2-4 调整名字的大小写
 
 name="echo li"
-print(name.title())
+
+# 首字母大写
+print(name.title()) 
+
+# 全部大写
+print(name.upper())
+
+# 全部小写
+print(name.lower())
 
 
