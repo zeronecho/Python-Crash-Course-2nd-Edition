adfdf6f389f9a8809767babbae961c03941e1430
zeronecho <zeronecho@outlook.com>
2022-12-26 14:12:16 +0800

    Add 练习4-1：披萨，列表循环

---
+++ Chapter4/4-1.py
@@ -0,0 +1,14 @@
+
+# 练习4-1：披萨
+
+pizza_list=['beef','chicken','cheese']
+for pizza in pizza_list:
+	print(pizza)
+
+for pizza in pizza_list:
+	print(f"I like {pizza.title()} flavour.")
+
+for pizza in pizza_list:
+	print(f"I really love {pizza} pizza.")
+
+print("I really love pizza!!!")
