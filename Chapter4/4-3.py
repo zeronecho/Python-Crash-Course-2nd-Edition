b11f2be66b0ab1fa39102cce8caab8019c95792b
zeronecho <zeronecho@outlook.com>
2023-01-03 23:59:49 +0800

    Add练习4-3:数到20,练习4-4:一百万

---
+++ Chapter4/4-3.py
@@ -0,0 +1,5 @@
+
+# 练习4-3:数到20
+
+for numbers in range(1,21):
+	print(numbers)
---
+++ Chapter4/4-4.py
@@ -0,0 +1,10 @@
+
+# 练习4-4:一百万
+
+
+# 创建列表
+number=list(range(1,1000001))
+
+# for 循环打印
+for numbers in number:
+	print(numbers)	
